536717f90bb6b1f63315877758a5c66b6acc2b5b
Artem Prokopev <mailzreguies@gmail.com>
2024-08-20 22:35:34 +0300

    Create base bot

--- src/config.py
+++ src/config.py
@@ -3,7 +3,3 @@ import os
 
 load_dotenv(".env")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-DB_HOST = os.environ.get("DB_HOST")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
