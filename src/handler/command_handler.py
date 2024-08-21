48af11b661750bb4341ca9723cafdb11687349cd
Artem Prokopev <mailzreguies@gmail.com>
2024-08-22 02:22:56 +0300

    Micro fix

--- src/handler/command_handler.py
+++ src/handler/command_handler.py
@@ -1,15 +1,11 @@
 from aiogram.types import Message
-from aiogram import Router, Bot
+from aiogram import Router
 from aiogram.filters import Command
 
 import keybord.inline_keybord as in_key
-
-
-#await bot.send_message(chat_id = -1002149371351, text = 'Link chat 1', reply_to_message_id = 1)
-#await bot.send_message(chat_id = -1002149371351, text = 'Link chat 2', reply_to_message_id = 4)
 
 routere_command = Router()
 
 @routere_command.message(Command('start'))
 async def start(message: Message):
-    await message.answer(f'Привет , в этом боте ты можешь подать обьявление о продажи, сдачи в аренду или о поиске недвижимости в Владивостоке', reply_markup = in_key.keybord_anketa )
+    await message.answer(f'Привет , в этом боте ты можешь подать обьявление о продажи, сдачи в аренду или о поиске недвижимости в Владивостоке\n\nЧто бы следить за новыми обьявлениями о продаже или аренде недвижимости, вам нужновступить в нашу 👉🏼 <a href="https://t.me/c/2149371351/4">группу</a> 👈🏼', reply_markup = in_key.keybord_anketa, parse_mode = 'HTML')
--- src/keybord/inline_keybord.py
+++ src/keybord/inline_keybord.py
@@ -1,17 +1,16 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 keybord_anketa = InlineKeyboardMarkup(inline_keyboard = [
-    [InlineKeyboardButton(text = 'Сдать\продать недвижимость', callback_data = 'anketa_arenda')],
-    [InlineKeyboardButton(text = 'Купить\арендовать недвижимость', callback_data = 'anketa_claim')]
+    [InlineKeyboardButton(text = 'Сдать\продать недвижимость 🏠', callback_data = 'anketa_arenda')],
+    [InlineKeyboardButton(text = 'Купить\арендовать недвижимость 🏘', callback_data = 'anketa_claim')]
 ])
 
 keybord_anketa_post = InlineKeyboardMarkup(inline_keyboard = [
-    [InlineKeyboardButton(text = 'Да', callback_data = 'post')],
-    [InlineKeyboardButton(text = 'Нет', callback_data = 'no_post')]
+    [InlineKeyboardButton(text = 'Да ✅', callback_data = 'post')],
+    [InlineKeyboardButton(text = 'Нет ❌', callback_data = 'no_post')]
 ])
 
 keybord_anketa_post_pull = InlineKeyboardMarkup(inline_keyboard = [
-    [InlineKeyboardButton(text = 'Да', callback_data = 'post_pull')],
-    [InlineKeyboardButton(text = 'Нет', callback_data = 'no_post_pull')]
+    [InlineKeyboardButton(text = 'Да ✅', callback_data = 'post_pull')],
+    [InlineKeyboardButton(text = 'Нет ❌', callback_data = 'no_post_pull')]
 ])
